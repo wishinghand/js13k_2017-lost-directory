06f607295ba9e4f79576a68d3ac361e7b46f8499
Gus Suarez <gus@origincodeacademy.com>
2017-08-16 18:59:55 -0700

    adds console

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-import Katalyst from './katalyst'
-import hero from './demo_hero'
+const c = document.getElementById('c')
 
-const canvas = document.getElementById('c')
-const imageSrc = 'images.png'
+const caret = '$'
 
-Katalyst(canvas, imageSrc, (game) => {
-  const menu = game.addScene('menu')
-  const group = menu.addGroup('hero')
-  group.addEntity(hero)
+const setState = (obj) => {
+  for (const key in obj) state[key] = obj[key]
+  console.log(state)
+  render(state)
+}
+
+const state = {
+  user: 'land',
+  host: 'lost',
+  path: '~',
+  lastDirPath: '',
+  value: ''
+}
+
+function getDirPath () {
+  return `${state.user}@${state.host}:${state.path}${caret} `
+}
+
+c.addEventListener('input', (e) => {
+  const dirPath = getDirPath()
+  setState({
+    lastDirPath: dirPath,
+    value: c.value.slice(dirPath.length)
+  })
 })
+
+function render (state) {
+  c.value = getDirPath() + state.value
+}
+
+render(state)
